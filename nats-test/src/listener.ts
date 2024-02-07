4fc98516108d7e40f82a50d89c744df4f87b4df0
damar <damarhuda@live.com>
2024-02-07 14:53:06 +0700

    nats streaming 2

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -10,15 +11,17 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
 	console.log('Listener connected to NATS');
 
-	const subscription = stan.subscribe('ticket:created');
-
-	subscription.on('message', (msg: Message) => {
-		const data = msg.getData();
-
-		if (typeof data === 'string') {
-			console.log(
-				`Receive event #${msg.getSequence()}, with data: ${data}`
-			);
-		}
+	stan.on('close', () => {
+		console.log('NATS connection closed!');
+		process.exit();
 	});
+
+	new TicketCreatedListener(stan).listen();
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
+
+
+
+
